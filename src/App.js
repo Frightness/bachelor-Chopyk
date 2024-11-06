c1000882be84a18b642f8b2d0281514bed03e227
Arthur <117268493+Frightness@users.noreply.github.com>
2024-11-06 04:59:58 +0200

    Completed the creation of the Main Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+import MainPage from "./Pages/MainPage/MainPage.js";
+import AuthPage from "./Pages/AuthPage/AuthPage.js";
+
+export default function App() {
   return (
-    <>
-    </>
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/auth" element={<AuthPage />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
-
-export default App;
---
+++ src/Pages/MainPage/MainPage.js
@@ -0,0 +1,56 @@
+import './MainPage.css';
+
+import Logo from "../../Assets/Logo.svg";
+import SpaceShip from "../../Assets/MainPageSpaceShip.svg";
+
+import { Box, Typography, Button } from '@mui/material';
+
+function createStars() {
+  const stars = [];
+  for (let i = 0; i < 30; i++) {
+    stars.push(
+      <div
+        key={i}
+        className="star"
+        style={{
+          top: `${Math.random() * 100}vh`,
+          left: `${Math.random() * 100}vw`,
+          animationDuration: `${Math.random() * 10 + 5}s`,
+          animationDelay: `${Math.random() * 5}s`,
+        }}
+      />
+    );
+  }
+  return stars;
+}
+
+export default function MainPage() {
+  return (
+    <Box className="wrapper">
+      <header>
+        <img src={Logo} draggable="false" />
+        <Box>
+          <Typography style={{ fontFamily: "Sigmar One", fontSize: "34px" }}>CINEMLY</Typography>
+          <Typography style={{ fontFamily: "Sigmar One", fontSize: "17px" }}>ENJOY TOGETHER</Typography>
+        </Box>
+      </header>
+
+      <main>
+        <Typography sx={{ fontFamily: "Sigmar One", fontSize: "40px", maxWidth: "628px", zIndex: "10" }}>
+          DISCOVER A UNIVERSE OF <span style={{ color: "#AD49E1" }}>VIDEO WITH FRIENDS</span>
+        </Typography>
+        <Typography sx={{ maxWidth: "560px", fontFamily: "Prosto One", fontSize: "16px", marginTop: "25px", zIndex: "10" }}>
+          We add variety and new emotions to virtual communication. 
+          Join us and make sure that distance is no longer an obstacle to watching films with your friends
+          or sharing happy family video memories!
+        </Typography>
+
+        <img src={SpaceShip} className="spaceShip" draggable="false" />
+
+        <Button variant="contained" className="startButton">Get Started</Button>
+      </main>
+
+      <div className="flyingStars">{createStars()}</div>
+    </Box>
+  );
+}
