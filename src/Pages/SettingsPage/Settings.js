06731a1b05b0be81ed1f61de28e344a258d9dccc
Arthur <117268493+Frightness@users.noreply.github.com>
2024-11-10 15:25:14 +0200

    Added back button in Settings Page

--- src/Pages/SettingsPage/Settings.js
+++ src/Pages/SettingsPage/Settings.js
@@ -1,11 +1,19 @@
 import "./Settings.css";
 import "./Responsive.css";
 import { Typography, Button, Box } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import BackArrow from "../../Assets/BackArrowIcon.svg";
 
 export default function SettingsPage() {
   return (
     <Box className="settingsWrapper">
-      <Typography className="mainText">Settings</Typography>
+      <Box className="head">
+        <Link to="/profile">
+          <img src={BackArrow} className="arrow" />
+        </Link>
+        <Typography className="mainText">Settings</Typography>
+      </Box>
 
       <Box className="changeBox">
         <Box className="changePasswordBox">
