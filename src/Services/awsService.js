55b7b3d7430bc1ffdf530f519fa196e8db317039
Arthur <117268493+Frightness@users.noreply.github.com>
2024-11-12 18:39:14 +0200

    Fixed some bugs with change avatar

--- src/Services/awsService.js
+++ src/Services/awsService.js
@@ -1,11 +1,11 @@
-import AWS from 'aws-sdk';
-import { getFirestore, doc, updateDoc } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import AWS from "aws-sdk";
+import { getFirestore, doc, getDoc, updateDoc } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 AWS.config.update({
   accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
-  region: process.env.REACT_APP_AWS_REGION
+  region: process.env.REACT_APP_AWS_REGION,
 });
 
 const s3 = new AWS.S3();
@@ -13,31 +13,66 @@ const db = getFirestore();
 const auth = getAuth();
 
 export async function uploadAvatar(file) {
+  const user = auth.currentUser;
+  let oldAvatarURL = "";
+
+  if (user) {
+    const userRef = doc(db, "users", user.uid);
+    const docSnap = await getDoc(userRef);
+
+    if (docSnap.exists()) {
+      oldAvatarURL = docSnap.data().avatarUrl;
+      console.log("Current avatar URL:", oldAvatarURL);
+
+      if (
+        oldAvatarURL &&
+        oldAvatarURL !=
+          "https://cinemly-users-uploaded-videos.s3.eu-north-1.amazonaws.com/avatars/defaultAvatar.svg"
+      ) {
+        const fileName = oldAvatarURL.split("/").pop();
+        await deleteAvatarFromS3(fileName);
+      }
+    } else {
+      console.log("No user data found.");
+    }
+  }
+
   const params = {
     Bucket: process.env.REACT_APP_S3_BUCKET_NAME,
-    Key: `avatars/${file.name}`, 
+    Key: `avatars/${file.name}`,
     Body: file,
-    ContentType: file.type
+    ContentType: file.type,
   };
 
   try {
     const data = await s3.upload(params).promise();
     const uploadURL = data.Location;
 
-    const user = auth.currentUser;
     if (user) {
-      const userRef = doc(db, 'users', user.uid);
+      const userRef = doc(db, "users", user.uid);
       await updateDoc(userRef, {
-        avatarUrl: uploadURL
+        avatarUrl: uploadURL,
       });
-      console.log('Avatar URL updated in Firebase');
-    } else {
-      console.log('No user is logged in');
+      console.log("Avatar URL updated in Firebase");
     }
 
     return uploadURL;
   } catch (error) {
     console.error("Error uploading file:", error);
     throw error;
+  }
+}
+
+async function deleteAvatarFromS3(fileName) {
+  const params = {
+    Bucket: process.env.REACT_APP_S3_BUCKET_NAME,
+    Key: `avatars/${fileName}`,
+  };
+
+  try {
+    await s3.deleteObject(params).promise();
+    console.log("Old avatar deleted from S3");
+  } catch (error) {
+    console.error("Error deleting old avatar:", error);
   }
 }
